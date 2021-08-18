a46ad65c4f41534326187a0381ebace342e0cc7c
Chooga <rumei.ma@gmail.com>
2021-08-18 09:36:04 +0200

    added Files

---
+++ engine/CMakeFiles/AtariAra.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/ALE/src/ale_interface.cpp" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/ale_interface.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/ale_interface.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/ALE/src/environment/ale_state.cpp" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/environment/ale_state.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/environment/ale_state.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/ALE/src/games/RomSettings.cpp" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/games/RomSettings.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/ALE/src/games/RomSettings.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/benchmark.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/benchmark.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/benchmark.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/bitbase.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/bitbase.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/bitbase.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/bitboard.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/bitboard.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/bitboard.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/endgame.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/endgame.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/endgame.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/evaluate.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/evaluate.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/evaluate.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/main.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/main.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/main.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/material.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/material.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/material.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/misc.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/misc.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/misc.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/movegen.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/movegen.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/movegen.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/movepick.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/movepick.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/movepick.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/pawns.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/pawns.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/pawns.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/position.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/position.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/position.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/psqt.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/psqt.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/psqt.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/search.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/search.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/search.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/syzygy/tbprobe.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/syzygy/tbprobe.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/syzygy/tbprobe.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/thread.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/thread.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/thread.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/timeman.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/timeman.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/timeman.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/tt.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/tt.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/tt.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/tune.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/tune.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/tune.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/uci.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/uci.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/uci.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/3rdparty/Stockfish/src/ucioption.cpp" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/ucioption.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/3rdparty/Stockfish/src/ucioption.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/agent.cpp" "CMakeFiles/AtariAra.dir/src/agents/agent.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/agent.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/config/playsettings.cpp" "CMakeFiles/AtariAra.dir/src/agents/config/playsettings.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/config/playsettings.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/config/rlsettings.cpp" "CMakeFiles/AtariAra.dir/src/agents/config/rlsettings.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/config/rlsettings.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/config/searchlimits.cpp" "CMakeFiles/AtariAra.dir/src/agents/config/searchlimits.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/config/searchlimits.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/config/searchsettings.cpp" "CMakeFiles/AtariAra.dir/src/agents/config/searchsettings.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/config/searchsettings.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/mctsagent.cpp" "CMakeFiles/AtariAra.dir/src/agents/mctsagent.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/mctsagent.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/mctsagentbatch.cpp" "CMakeFiles/AtariAra.dir/src/agents/mctsagentbatch.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/mctsagentbatch.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/mctsagenttruesight.cpp" "CMakeFiles/AtariAra.dir/src/agents/mctsagenttruesight.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/mctsagenttruesight.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/randomagent.cpp" "CMakeFiles/AtariAra.dir/src/agents/randomagent.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/randomagent.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/rawnetagent.cpp" "CMakeFiles/AtariAra.dir/src/agents/rawnetagent.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/rawnetagent.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/agents/util/gcthread.cpp" "CMakeFiles/AtariAra.dir/src/agents/util/gcthread.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/agents/util/gcthread.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/environments/atari/ataristate.cpp" "CMakeFiles/AtariAra.dir/src/environments/atari/ataristate.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/environments/atari/ataristate.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/environments/atari/main.cpp" "CMakeFiles/AtariAra.dir/src/environments/atari/main.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/environments/atari/main.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/environments/atari/sharedLibraryInterfaceWithModesExample.cpp" "CMakeFiles/AtariAra.dir/src/environments/atari/sharedLibraryInterfaceWithModesExample.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/environments/atari/sharedLibraryInterfaceWithModesExample.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/evalinfo.cpp" "CMakeFiles/AtariAra.dir/src/evalinfo.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/evalinfo.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/manager/threadmanager.cpp" "CMakeFiles/AtariAra.dir/src/manager/threadmanager.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/manager/threadmanager.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/manager/timemanager.cpp" "CMakeFiles/AtariAra.dir/src/manager/timemanager.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/manager/timemanager.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/manager/treemanager.cpp" "CMakeFiles/AtariAra.dir/src/manager/treemanager.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/manager/treemanager.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/mxnetapi.cpp" "CMakeFiles/AtariAra.dir/src/nn/mxnetapi.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/mxnetapi.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/neuralnetapi.cpp" "CMakeFiles/AtariAra.dir/src/nn/neuralnetapi.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/neuralnetapi.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/neuralnetapiuser.cpp" "CMakeFiles/AtariAra.dir/src/nn/neuralnetapiuser.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/neuralnetapiuser.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/neuralnetdesign.cpp" "CMakeFiles/AtariAra.dir/src/nn/neuralnetdesign.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/neuralnetdesign.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/tensorrtapi.cpp" "CMakeFiles/AtariAra.dir/src/nn/tensorrtapi.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/tensorrtapi.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nn/torchapi.cpp" "CMakeFiles/AtariAra.dir/src/nn/torchapi.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nn/torchapi.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/node.cpp" "CMakeFiles/AtariAra.dir/src/node.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/node.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/nodedata.cpp" "CMakeFiles/AtariAra.dir/src/nodedata.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/nodedata.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/rl/gamepgn.cpp" "CMakeFiles/AtariAra.dir/src/rl/gamepgn.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/rl/gamepgn.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/rl/selfplay.cpp" "CMakeFiles/AtariAra.dir/src/rl/selfplay.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/rl/selfplay.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/rl/tournamentresult.cpp" "CMakeFiles/AtariAra.dir/src/rl/tournamentresult.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/rl/tournamentresult.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/rl/traindataexporter.cpp" "CMakeFiles/AtariAra.dir/src/rl/traindataexporter.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/rl/traindataexporter.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/searchthread.cpp" "CMakeFiles/AtariAra.dir/src/searchthread.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/searchthread.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/state.cpp" "CMakeFiles/AtariAra.dir/src/state.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/state.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/stateobj.cpp" "CMakeFiles/AtariAra.dir/src/stateobj.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/stateobj.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/uci/crazyara.cpp" "CMakeFiles/AtariAra.dir/src/uci/crazyara.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/uci/crazyara.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/uci/customlogger.cpp" "CMakeFiles/AtariAra.dir/src/uci/customlogger.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/uci/customlogger.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/uci/main.cpp" "CMakeFiles/AtariAra.dir/src/uci/main.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/uci/main.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/uci/optionsuci.cpp" "CMakeFiles/AtariAra.dir/src/uci/optionsuci.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/uci/optionsuci.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/uci/timeoutreadythread.cpp" "CMakeFiles/AtariAra.dir/src/uci/timeoutreadythread.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/uci/timeoutreadythread.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/util/blazeutil.cpp" "CMakeFiles/AtariAra.dir/src/util/blazeutil.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/util/blazeutil.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/util/communication.cpp" "CMakeFiles/AtariAra.dir/src/util/communication.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/util/communication.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/src/util/randomgen.cpp" "CMakeFiles/AtariAra.dir/src/util/randomgen.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/src/util/randomgen.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/tests/benchmarkpositions.cpp" "CMakeFiles/AtariAra.dir/tests/benchmarkpositions.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/tests/benchmarkpositions.cpp.o.d"
+  "/home/chooga/uni/AtariAra/CrazyAra/engine/tests/tests.cpp" "CMakeFiles/AtariAra.dir/tests/tests.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/tests/tests.cpp.o.d"
+  "/usr/src/tensorrt/samples/common/logger.cpp" "CMakeFiles/AtariAra.dir/usr/src/tensorrt/samples/common/logger.cpp.o" "gcc" "CMakeFiles/AtariAra.dir/usr/src/tensorrt/samples/common/logger.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ engine/src/environments/atari/main.cpp
@@ -0,0 +1,140 @@
+/* *****************************************************************************
+ * A.L.E (Arcade Learning Environment)
+ * Copyright (c) 2009-2013 by Yavar Naddaf, Joel Veness, Marc G. Bellemare,
+ *  Matthew Hausknecht, and the Reinforcement Learning and Artificial Intelligence
+ *  Laboratory
+ * Released under the GNU General Public License; see License.txt for details.
+ *
+ * Based on: Stella  --  "An Atari 2600 VCS Emulator"
+ * Copyright (c) 1995-2007 by Bradford W. Mott and the Stella team
+ *
+ * *****************************************************************************
+ *  sharedLibraryInterfaceExample.cpp
+ *
+ *  Sample code for running an agent with the shared library interface.
+ **************************************************************************** */
+
+#include <iostream>
+#include <ale_interface.hpp>
+#include <filesystem>
+#include "ataristate.h"
+#include "agents/rawnetagent.h"
+#include "agents/mctsagent.h"
+#include "nn/tensorrtapi.h"
+#include "uci/optionsuci.h"
+#ifdef __USE_SDL
+#include <SDL.h>
+#endif
+
+
+
+int main(int argc, char** argv) {
+
+    SearchSettings ss;
+    SearchLimits sl;
+    PlaySettings ps;
+    EvalInfo ei;
+    AtariState atariState;
+    atariState.init(0, false);
+    float customReward;
+
+
+
+
+//    srand(123);
+//    AtariState state;
+//    state.init(0, false);
+//    for(int i = 0; i <= 100; i++) {
+//        state.do_action((rand()%17+1));
+//    }
+//    unique_ptr<AtariState> state2 = unique_ptr<AtariState>(state.clone());
+//    if(state2->getTotalReward() == state.getTotalReward())
+//        printf("clone working!\n");
+//    else
+//        printf("new state: %e, old state: %e \n",state2->getTotalReward(), state.getTotalReward());
+
+
+
+    cout << "game ended: " << customReward << endl;
+    TensorrtAPI nn(0, 1, "../model", "float32");
+//    RawNetAgent rna(&nn,&ps,true);
+    vector<unique_ptr<NeuralNetAPI>> netBatches;
+    for (int deviceId = 0; deviceId <= 0; ++deviceId) {
+        for (size_t i = 0; i < ss.threads; ++i) {
+            netBatches.push_back(make_unique<TensorrtAPI>(deviceId, ss.batchSize, "../model", "float32"));
+        }
+    }
+    MCTSAgent rna(&nn, netBatches ,&ss, &ps);
+    rna.set_search_settings(&atariState,&sl,&ei);
+    while (atariState.is_terminal(0,customReward) == TERMINAL_NONE) {
+        rna.perform_action();
+        cout << ei.bestMove << endl;
+        atariState.do_action(ei.bestMove);
+    }
+
+//  if (argc < 2) {
+//    std::cerr << "Usage: " << argv[0] << " rom_file" << std::endl;
+//    return 1;
+//  }
+
+//  ale::ALEInterface ale;
+
+//  // Get & Set the desired settings
+//  ale.setInt("random_seed", 123);
+//  //The default is already 0.25, this is just an example
+//  ale.setFloat("repeat_action_probability", 0.25);
+
+//#ifdef __USE_SDL
+//  ale.setBool("display_screen", true);
+//  ale.setBool("sound", true);
+//#endif
+
+//  // Load the ROM file. (Also resets the system for new settings to
+//  // take effect.)
+//  ale.loadROM(argv[1]);
+
+//  // Get the vector of legal actions
+//  ale::ActionVect legal_actions = ale.getLegalActionSet();
+
+//  // Play 10 episodes
+//  for (int episode = 0; episode < 10; episode++) {
+//    float totalReward = 0;
+//    while (!ale.game_over()) {
+//      ale::Action a = legal_actions[rand() % legal_actions.size()];
+//      // Apply the action and get the resulting reward
+//      float reward = ale.act(a);
+//      totalReward += reward;
+//    }
+//    cout << "Episode " << episode << " ended with score: " << totalReward
+//         << endl;
+//    ale.reset_game();
+//  }
+  return 0;
+}
+
+
+//Things to test:
+//    assert(theOSystem.get());
+//    if (rom_file.empty()) {
+//      rom_file = theOSystem->romFile();
+//    }
+
+//    // Load all settings corresponding to the ROM file and create a new game
+//    // console, with attached devices, capable of emulating the ROM.
+//    loadSettings(rom_file, theOSystem);
+
+//    const Properties properties = theOSystem->console().properties();
+//    const std::string md5 = properties.get(Cartridge_MD5);
+//    const std::string name = properties.get(Cartridge_Name);
+
+//    RomSettings* wrapper = buildRomRLWrapper(rom_file, md5);
+
+//    romSettings.reset(wrapper);
+
+//    // Custom environment settings required for a specific ROM must be added
+//    // before the StellaEnvironment is constructed.
+//    romSettings->modifyEnvironmentSettings(theOSystem->settings());
+
+//    environment.reset(new StellaEnvironment(theOSystem.get(), romSettings.get()));
+//    max_num_frames = theOSystem->settings().getInt("max_num_frames_per_episode");
+//    environment->reset();
