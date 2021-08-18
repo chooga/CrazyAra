0fda13f2bca05e0a81ce9d6e03bb8e31f0c8f8c2
Chooga <rumei.ma@gmail.com>
2021-08-18 09:32:16 +0200

    Atari support with Arcade Learning Environment linked

---
+++ engine/3rdparty/ALE/examples/cpp-interface/CMakeFiles/sharedLibraryInterfaceExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharedLibraryInterfaceExample.dir/sharedLibraryInterfaceExample.cpp.o"
+  "CMakeFiles/sharedLibraryInterfaceExample.dir/sharedLibraryInterfaceExample.cpp.o.d"
+  "sharedLibraryInterfaceExample"
+  "sharedLibraryInterfaceExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharedLibraryInterfaceExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ engine/3rdparty/ALE/examples/cpp-interface/CMakeFiles/sharedLibraryInterfaceWithModesExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharedLibraryInterfaceWithModesExample.dir/sharedLibraryInterfaceWithModesExample.cpp.o"
+  "CMakeFiles/sharedLibraryInterfaceWithModesExample.dir/sharedLibraryInterfaceWithModesExample.cpp.o.d"
+  "sharedLibraryInterfaceWithModesExample"
+  "sharedLibraryInterfaceWithModesExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharedLibraryInterfaceWithModesExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
